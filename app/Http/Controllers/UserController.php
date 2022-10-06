873b03dafffee14ba0e5e4b4b55fa5f38a1ac5e8
= <=>
2022-10-06 13:49:55 -0300

    Implementação de novos metodos

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreUpdateFormRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
+use PhpParser\Node\Stmt\Return_;
 
 class UserController extends Controller
 {
@@ -20,5 +22,43 @@ class UserController extends Controller
             return redirect()->route('users.index');
         
         return view('users.show',compact('user'));
+    }
+
+    public function create()
+    {
+        return view('users.create');
+    }
+
+    public function store(StoreUpdateFormRequest $request)
+    {
+        $data = $request->all();
+        $data['password'] = bcrypt($request->password);
+        User::create($data);
+
+        return redirect()->route('users.index');
+    }
+
+    public function edit($id)
+    {
+        if(!$user = User::find($id))
+        return redirect()->route('users.index');
+
+        return view('users.edit',compact('user'));
+
+    }
+
+    public function update(StoreUpdateFormRequest $request ,$id)
+    {
+        if(!$user = User::find($id))
+        return redirect()->route('users.index');
+    
+        $data = $request->only('name','email');  
+        
+        if($request->password)
+            $data['password'] = bcrypt($request->password);
+        $user->update($data);
+
+        return redirect()->route('users.index');   
+
     }
 }
