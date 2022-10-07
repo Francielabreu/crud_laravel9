04920dbefea85fd51717ac73114db60e2eed4025
= <=>
2022-10-07 11:16:14 -0300

    Implementação de novas rotas

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,7 @@ Route::get('/users',[UserController::class, 'index'])->name('users.index');
 Route::get('/users/{id}',[UserController::class, 'show'])->name('users.show');
 Route::get('/users/{id}/edit',[UserController::class,'edit'])->name('users.edit');
 Route::put('/users/{id}',[UserController::class,'update'])->name('users.update');
+Route::delete('/users/{id}',[UserController::class,'destroy'])->name('users.destroy');
 
 Route::get('/', function () {
     return view('welcome');
