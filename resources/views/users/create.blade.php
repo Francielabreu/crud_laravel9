58fe1d7536e0ca2ef6daa24f94f06cbdc8fad6d6
= <=>
2022-10-07 11:20:21 -0300

    Componentizei o formulario

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -2,44 +2,16 @@
 @section('title', 'Cadastro de Usuarios')
 
 @section('content')
-
     <h1 class="text-4xl font-bold leading-tigh py-6 text-center uppercase">Novo Usuário</h1>
 
-    @if ($errors->any())
-        <ul class="errors">
-            @foreach ($errors->all() as $error)
-                <li class="error">{{ $error }}</li>
-            @endforeach
-        </ul>
-    @endif
+    @include('validate.error')
 
     <form action="{{ route('users.store') }}" method="post" class="my-8 px-4 max-w-3xl mx-auto bg-gray-100">
-        @csrf
-
-        <div class="py-2 space-y-4">
-            <div class="space-y-1">
-                <label class="text-gray-600 font-bold">Nome</label>
-                <input type="text" name="name" placeholder="Informe seu Nome"
-                    class="border border-gray-400 px-4 py-1 rounded block w-full" value="{{ old('name') }}">
-            </div>
-
-            <div class="space-y-1">
-                <label class="text-gray-600 font-bold">E-mail</label>
-                <input type="email" name="email" placeholder="Informe seu E-mail"
-                    class=" border border-gray-400 px-4 py-1 rounded block w-full" value="{{ old('email') }}">
-            </div>
-
-            <div class="space-y-1">
-                <label class="text-gray-600 font-bold">Password</label>
-                <input type="password" name="password" placeholder="Informe o Password"
-                    class=" border border-gray-400 px-4 py-1 rounded block w-full">
-            </div>
-        </div>
+        
+        @include('users.partials.forms')
 
         <div div class="py-4">
-            <button type="submit"
-                class="w-full shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus: outline-none text-white font-bold py-2  px-4 py-1 rounded uppercase">Cadastrar</button>
+            <button type="submit" class="w-full shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus: outline-none text-white font-bold py-2  px-4 py-1 rounded uppercase">Cadastrar</button>
         </div>
     </form>
-
 @endsection
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -4,36 +4,13 @@
 @section('content')
 <h1 class="text-4xl font-bold leading-tigh py-6 text-center uppercase">Detalhes do Usuario: {{ $user->name }}</h1>
 
-
 <form action="#" method="post" class="my-8 px-4 max-w-3xl mx-auto bg-gray-100">
-    @csrf
+    @include('users.partials.forms')
 
-    
-    <div class="py-2 space-y-4">
-        <div class="space-y-1">
-            <label class="text-gray-600 font-bold">Nome</label>
-            <input type="text" name="name" placeholder="Informe seu Nome"
-                class="border border-gray-400 px-4 py-1 rounded block w-full" disabled="disabled" value="{{ $user->name}}">
-        </div>
-
-        <div class="space-y-1">
-            <label class="text-gray-600 font-bold">E-mail</label>
-            <input type="email" name="email" placeholder="Informe seu E-mail"
-                class=" border border-gray-400 px-4 py-1 rounded block w-full" disabled="disabled" value="{{ $user->email }}">
-        </div>
-
-        <div class="space-y-1">
-            <label class="text-gray-600 font-bold">Password</label>
-            <input type="password" name="password" placeholder="Informe o Password"
-                class=" border border-gray-400 px-4 py-1 rounded block w-full" disabled="disabled" value="{{ $user->password }}">
-        </div>
-    </div>
-
-    <div div class="py-4">
-        <button type="submit"
-            class="w-full shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus: outline-none text-white font-bold py-2 uppercase px-4 py-1 rounded">Deletar</button>
-    </div>
+    <form action="{{ route('users.destroy',$user->id) }}" method="post" class="py-4">
+        @csrf
+        @method('DELETE')
+        <button type="submit" class="w-full shadow bg-red-500 hover:bg-red-400 focus:shadow-outline focus: outline-none text-white font-bold py-2 uppercase px-4 py-1 rounded">Deletar</button>
+    </form>
 </form>
-
-
 @endsection
